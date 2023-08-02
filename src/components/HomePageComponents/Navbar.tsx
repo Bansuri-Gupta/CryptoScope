36bcbf6e2a7c598c6d6c0e4974fa8c715b9a36ca
Bansuri gupta <108368737+Bansuri-Gupta@users.noreply.github.com>
2023-08-02 19:33:26 +0530

    Update Navbar.tsx

--- src/components/HomePageComponents/Navbar.tsx
+++ src/components/HomePageComponents/Navbar.tsx
@@ -36,12 +36,6 @@ const Navbar = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
               </label>
               <div className='w-32 sm:w-56'>
-
-                {/* <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                  <li><a>Homepage</a></li>
-                  <li><a>Portfolio</a></li>
-                  <li><a>About</a></li>
-                </ul> */}
               </div>
 
             </div>
